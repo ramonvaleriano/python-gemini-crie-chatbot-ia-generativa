d18708000407fceb7fd0410015bf00f623aa23ba
Ramon Valeriano <rrvaleriano@gmail.com>
2025-09-08 15:33:53 -0300

    feat(service/bot.py): Criando histório de conversar para o chatbot

--- service/bot.py
+++ service/bot.py
@@ -7,11 +7,45 @@ from service.google_genai import GoogleGenai
 from service.selecionar_persona import SelecionarPersona
 
 
+contexto = carrega("data/musimart.txt")
+
+def cria_chatbot():
+    personalidade = "neutro"
+
+    prompt_do_sistema = f"""
+    # PERSONA
+    Você é um chatbot de atendimento a clientes de um e-commerce. 
+    Você não deve responder perguntas que não sejam dados do ecommerce informado!
+    
+    Você deve utilizar apenas dados que estejam dentro do 'contexto'
+
+    # CONTEXTO
+    {contexto}
+    
+    # PERSONALIDADE
+    {personalidade}
+
+    # Histórico
+    Acesse sempre o histórico de mensagens, e recupere informações ditas anteriormente.
+    """
+
+    configuracao_modelo = {"temperature": 0.1, "max_output_tokens": 8192}
+
+    print("\nPreparando para envio para a GEMINI")
+    llm_genai = GoogleGenai(
+        system_inproduction=prompt_do_sistema,
+        generation_config=configuracao_modelo,
+    )
+    
+    chatbot = llm_genai.llm.start_chat(history=[])
+
+    return chatbot
+
+chatbot = cria_chatbot()
+
 def bot(prompt):
     maximo_tentativas = 1
     repeticao = 0
-
-    contexto = carrega("data/musimart.txt")
 
     while True:
         try:
@@ -19,30 +53,16 @@ def bot(prompt):
             print(f"Sentimento gerado {sentimento}")
             personalidade = personas[sentimento]
 
-            prompt_do_sistema = f"""
-            # PERSONA
-            Você é um chatbot de atendimento a clientes de um e-commerce. 
-            Você não deve responder perguntas que não sejam dados do ecommerce informado!
-            
-            Você deve utilizar apenas dados que estejam dentro do 'contexto'
+            mwensagem_usuario = f"""
+            Considere está personalidade para responder a mensagem: {personalidade}
 
-            # CONTEXTO
-            {contexto}
-            
-            # PERSONALIDADE
-            {personalidade}
+            responda a seguinte mensagem, sempre lembrando do histórico:
+            {prompt}
             """
 
-            configuracao_modelo = {"temperature": 0.1, "max_output_tokens": 8192}
+            response = chatbot.send_message(mwensagem_usuario)
 
-            print("\nPreparando para envio para a GEMINI")
-            llm_genai = GoogleGenai(
-                system_inproduction=prompt_do_sistema,
-                generation_config=configuracao_modelo,
-            )
-            resposta = llm_genai.context_response(context=prompt)
-
-            return resposta.text
+            return response.text
 
         except Exception as erro:
             repeticao += 1
