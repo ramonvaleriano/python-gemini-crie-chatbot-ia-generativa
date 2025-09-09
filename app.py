6beb6c161591cf0f0fe2da30de7b081e47f974ea
Ramon Valeriano <rrvaleriano@gmail.com>
2025-09-08 21:32:30 -0300

    chore(app.py): Ajustando código para salvar imagens

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import uuid
 from flask import Flask,render_template, request, Response
 from flask_cors import CORS
 
@@ -6,6 +8,22 @@ from service.bot import bot
 app = Flask(__name__)
 CORS(app)
 app.secret_key = 'alura'
+
+caminho_imagem_enviada = None
+UPLOAD_FOLDER = "imagens_temporarias"
+
+@app.route("/upload_imagem", methods=["POST"])
+def upload_imagem():
+    global caminho_imagem_enviada
+
+    if "imagem" in request.files:
+        imagem_enviada = request.files["imagem"]
+        nome_arquivo = str(uuid.uuid4()) + os.path.splitext(imagem_enviada.filename)[1]
+        caminho_arquivo = os.path.join(UPLOAD_FOLDER, nome_arquivo)
+        imagem_enviada.save(caminho_arquivo)
+        return "Imagem enviada com sucesso", 200
+    return "Nenhum arquivo enviado", 400
+
 
 @app.route("/chat", methods=["POST"])
 def chat():
